20fa9e6115c30041b62a430f98baa795be1444b4
gigakhrz <kharazishvilig13@gmail.com>
2023-12-19 16:31:43 +0400

    create second helf which will bottom position after blaack background

---
+++ components/firstHalfChilds/secondHelf.tsx
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+
+const secondHelf = (): JSX.Element => {
+  return <></>;
+};
+
+export default secondHelf;
+
+const style = StyleSheet.create({
+  sHWrapper: {},
+});
