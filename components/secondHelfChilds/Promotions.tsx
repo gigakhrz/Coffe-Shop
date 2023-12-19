0851d92f5381f3970e45795c332ac47905648579
gigakhrz <kharazishvilig13@gmail.com>
2023-12-19 16:49:03 +0400

    create component body

---
+++ components/secondHelfChilds/Promotions.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native';
+
+const Promotions = (): JSX.Element => {
+  return <></>;
+};
+
+export default Promotions;
+
+const styles = StyleSheet.create({
+  PromptionWrapper: {
+    width: 315,
+    height: 140,
+  },
+});
