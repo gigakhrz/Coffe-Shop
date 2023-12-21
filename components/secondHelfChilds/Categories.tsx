4b040dbf49b4bf9235d6e82f858edfa80057e40d
gigakhrz <kharazishvilig13@gmail.com>
2023-12-21 15:35:20 +0400

    create categories file

---
+++ components/secondHelfChilds/Categories.tsx
@@ -0,0 +1,11 @@
+import {StyleSheet, View} from 'react-native';
+
+const Categories = (): JSX.Element => {
+  return <View></View>;
+};
+
+export default Categories;
+
+const styles = StyleSheet.create({
+  CategoriesWrapper: {},
+});
