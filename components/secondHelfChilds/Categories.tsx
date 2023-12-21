09a7c81e033b1208c86d1496ee773d4b5d5f63de
gigakhrz <kharazishvilig13@gmail.com>
2023-12-21 15:45:31 +0400

    trying to add inline categoryes

--- components/secondHelfChilds/Categories.tsx
+++ components/secondHelfChilds/Categories.tsx
@@ -1,7 +1,20 @@
-import {ScrollView, StyleSheet, View} from 'react-native';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import data from '../../data.json';
 
 const Categories = (): JSX.Element => {
-  return <ScrollView></ScrollView>;
+  console.log(data.coffee_categories);
+
+  return (
+    <ScrollView style={styles.CategoriesWrapper}>
+      {data.coffee_categories.map(item => {
+        return (
+          <View style={styles.category}>
+            <Text style={styles.text}>{item.category}</Text>
+          </View>
+        );
+      })}
+    </ScrollView>
+  );
 };
 
 export default Categories;
@@ -9,7 +22,20 @@ export default Categories;
 const styles = StyleSheet.create({
   CategoriesWrapper: {
     display: 'flex',
+    flexDirection: 'row',
+    gap: 7,
+  },
+
+  category: {
+    backgroundColor: '#F3F3F3',
+    display: 'flex',
     alignItems: 'center',
-    backgroundColor: 'black',
+    padding: 10,
+  },
+
+  text: {
+    color: '#2F4B4E',
+    fontSize: 14,
+    fontWeight: '400',
   },
 });
