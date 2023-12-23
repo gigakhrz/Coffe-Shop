ca777d5c7e7b6f807b8206d7d1197f50e0cffa85
gigakhrz <kharazishvilig13@gmail.com>
2023-12-23 11:56:42 +0400

    add some styles

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,19 @@
 import {StyleSheet, View} from 'react-native';
 
 const Footer = (): JSX.Element => {
-  return <View></View>;
+  return <View style={styles.footerWrapper}></View>;
 };
 
 const styles = StyleSheet.create({
-  footerWrapper: {},
+  footerWrapper: {
+    alignSelf: 'flex-end',
+    width: '100%',
+    backgroundColor: '#F9F9F9',
+    height: 94,
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: '#F1F1F1',
+  },
 });
 
 export default Footer;
