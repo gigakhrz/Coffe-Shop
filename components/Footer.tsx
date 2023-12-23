66b77b41fa35f6ad932a0993e2c29435c6bcbb57
gigakhrz <kharazishvilig13@gmail.com>
2023-12-23 12:13:25 +0400

    add home icon

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,6 +3,7 @@ import {Image, StyleSheet, View} from 'react-native';
 const Footer = (): JSX.Element => {
   return (
     <View style={styles.footerWrapper}>
+      <Image style={styles.home} source={require('../assets/Home.png')} />
       <Image style={styles.home} source={require('../assets/Bag2.png')} />
     </View>
   );
@@ -17,11 +18,16 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     borderWidth: 1,
     borderColor: '#F1F1F1',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 60,
   },
 
   home: {
-    minWidth: 19,
-    minHeight: 19,
+    minWidth: 30,
+    minHeight: 30,
   },
 });
 
