fcf07fd0921f287bb9eb0b26af3d0cba566a1a87
gigakhrz <kharazishvilig13@gmail.com>
2023-12-23 12:16:49 +0400

    create default Basket component

---
+++ components/Basket.tsx
@@ -0,0 +1,11 @@
+import {StyleSheet, View} from 'react-native';
+
+const Basket = (): JSX.Element => {
+  return <View></View>;
+};
+
+const styles = StyleSheet.create({
+  basktWrapper: {},
+});
+
+export default Basket;
