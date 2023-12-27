467f547a32e1bf35aa1757dbd7b4ecc54cf1aa42
gigakhrz <kharazishvilig13@gmail.com>
2023-12-27 17:14:30 +0400

    A certain amount of progress in navigator

--- App.tsx
+++ App.tsx
@@ -2,8 +2,15 @@ import {StyleSheet, View} from 'react-native';
 import Welcome from './components/mainPageChilds/Welcome';
 import {useEffect, useState} from 'react';
 import Footer from './components/Footer';
-// import AppNavigator from './components/AppNavigator';
+// screens
 import MainPage from './components/MainPage';
+import Basket from './components/Basket';
+// navigators
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {RootStackParamList} from './type';
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function App(): React.JSX.Element {
   const [welcome, setWelcome] = useState<boolean>(false);
@@ -18,9 +25,17 @@ function App(): React.JSX.Element {
 
   return (
     <View style={{flex: 1}}>
-      {welcome ? <MainPage /> : <Welcome />}
-      {/* shevasworoot */}
-      <Footer />
+      {welcome ? (
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="MainPage">
+            <Stack.Screen name="MainPage" component={MainPage} />
+            <Stack.Screen name="Basket" component={Basket} />
+          </Stack.Navigator>
+          <Footer />
+        </NavigationContainer>
+      ) : (
+        <Welcome />
+      )}
     </View>
   );
 }
