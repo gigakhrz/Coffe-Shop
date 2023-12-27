3eb490030977ea08c32b87c331803277b59a7681
gigakhrz <kharazishvilig13@gmail.com>
2023-12-27 10:00:43 +0400

    delete unnecessary files

--- App.tsx
+++ App.tsx
@@ -3,7 +3,6 @@ import Welcome from './components/mainPageChilds/Welcome';
 import {useEffect, useState} from 'react';
 import Footer from './components/Footer';
 import AppNavigator from './components/AppNavigator';
-import {useNavigation} from '@react-navigation/native';
 
 function App(): React.JSX.Element {
   const [welcome, setWelcome] = useState<boolean>(false);
