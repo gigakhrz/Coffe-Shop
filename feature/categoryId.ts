e86228838399d7349573d900ac65721c130d9c22
gigakhrz <kharazishvilig13@gmail.com>
2023-12-29 11:04:28 +0400

    create category id redux state

---
+++ feature/categoryId.ts
@@ -0,0 +1,22 @@
+import {PayloadAction, createSlice} from '@reduxjs/toolkit';
+
+interface CagatoryProps {
+  id: number;
+}
+
+const initialState: CagatoryProps = {
+  id: 1,
+};
+
+const categoryId = createSlice({
+  name: 'categoryId',
+  initialState,
+  reducers: {
+    setCategoryId: (state, action: PayloadAction<number>) => {
+      state.id = action.payload;
+    },
+  },
+});
+
+export const {setCategoryId} = categoryId.actions;
+export default categoryId.reducer;
