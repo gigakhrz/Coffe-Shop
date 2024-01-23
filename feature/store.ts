586ab21eb8b4a231179f564a681d4b8479e99014
gigakhrz <kharazishvilig13@gmail.com>
2024-01-23 23:06:15 +0400

    add the basket product slice in store

--- feature/store.ts
+++ feature/store.ts
@@ -2,12 +2,15 @@ import {configureStore} from '@reduxjs/toolkit';
 import searchTitleSlice, {TitleProps} from './searchTitleSlice';
 import categoryIdSlice, {CagatoryProps} from './categoryIdSlice';
 import filteredCoffesSlice, {FilteredProps} from './filteredCoffesSlice';
+import basketProductsSlice from './basketProductsSlice';
+import {BaseBackgroundPropType} from 'react-native';
 
 const store = configureStore({
   reducer: {
     categoryId: categoryIdSlice,
     search: searchTitleSlice,
     filteredCoffe: filteredCoffesSlice,
+    basketProducts: basketProductsSlice,
   },
 });
 
@@ -15,6 +18,7 @@ export type RootState = {
   categoryId: CagatoryProps;
   search: TitleProps;
   filteredCoffe: FilteredProps;
+  basketProducts: BaseBackgroundPropType;
 };
 
 export default store;
