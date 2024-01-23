f2cccd4f802af4fa32bd42227e25ea48ae11366c
gigakhrz <kharazishvilig13@gmail.com>
2024-01-23 23:04:47 +0400

    create basketProductSlice that is where product id will add when user click on + button, then we will filter defaultproduct data based on this state to display that added products

---
+++ feature/basketProductsSlice.ts
@@ -0,0 +1,27 @@
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+
+interface BasketProductProps {
+  products: number[];
+}
+
+const initialState: BasketProductProps = {
+  products: [],
+};
+
+const basketProductsSlice = createSlice({
+  name: 'basketProducts',
+  initialState,
+  reducers: {
+    setProducts: (state, action: PayloadAction<number>) => {
+      const productId = action.payload;
+      if (state.products.includes(productId)) {
+        state.products;
+      } else {
+        state.products.push(productId);
+      }
+    },
+  },
+});
+
+export const {setProducts} = basketProductsSlice.actions;
+export default basketProductsSlice.reducer;
