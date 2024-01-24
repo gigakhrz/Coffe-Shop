9d12ce439ca33b098601c39666011b40b1e2d7a1
gigakhrz <kharazishvilig13@gmail.com>
2024-01-24 19:55:49 +0400

    add some styles

--- components/detailsChilds/CoffeInfo.tsx
+++ components/detailsChilds/CoffeInfo.tsx
@@ -10,8 +10,17 @@ const CoffeInfo = ({image, title, rate}: CoffeInfoProps): JSX.Element => {
   return (
     <View style={styles.wrapper}>
       <Image style={styles.image} source={{uri: image}} />
-      <View>
-        <View></View>
+      <View style={styles.textWrapper}>
+        <View>
+          <Text style={styles.title}>{title}</Text>
+          <View style={styles.rateWrapper}>
+            <Image
+              style={styles.star}
+              source={require('../../assets/star.png')}
+            />
+            <Text style={styles.rate}>{rate}</Text>
+          </View>
+        </View>
         <View></View>
       </View>
     </View>
@@ -27,6 +36,42 @@ const styles = StyleSheet.create({
   image: {
     width: '100%',
     height: 226,
+  },
+
+  textWrapper: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: '100%',
+    height: 150,
+  },
+
+  titleRateWraper: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+
+  title: {
+    color: '#2F2D2C',
+    fontSize: 20,
+    fontWeight: '600',
+  },
+
+  rateWrapper: {
+    display: 'flex',
+    gap: 4,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+
+  star: {
+    width: 20,
+    height: 20,
+  },
+
+  rate: {
+    color: '#2F2D2C',
+    fontSize: 16,
+    fontWeight: '600',
   },
 });
 
