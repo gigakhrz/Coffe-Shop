da749a6d250344354340c91319eec40a2642f5d7
gigakhrz <kharazishvilig13@gmail.com>
2024-01-27 17:10:39 +0400

    create basket

---
+++ components/basket/Basket.tsx
@@ -0,0 +1,10 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const Basket = () => {
+  return <View></View>;
+};
+
+export default Basket;
+
+const styles = StyleSheet.create({});
