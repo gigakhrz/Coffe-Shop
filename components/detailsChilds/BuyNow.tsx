f02c0a336178bfb68826dfb03d82f366585516b1
gigakhrz <kharazishvilig13@gmail.com>
2024-01-27 16:27:23 +0400

    create buynow markup

--- components/detailsChilds/BuyNow.tsx
+++ components/detailsChilds/BuyNow.tsx
@@ -9,11 +9,11 @@ const BuyNow = ({itemId, price}: BuyNowProps): JSX.Element => {
   return (
     <View style={styles.wrapper}>
       <View style={styles.priceWrapper}>
-        <Text>Price</Text>
-        <Text></Text>
+        <Text style={styles.priceText}>Price</Text>
+        <Text style={styles.priceNumber}>{price}</Text>
       </View>
-      <TouchableOpacity>
-        <Text>Buy Now</Text>
+      <TouchableOpacity style={styles.touchable}>
+        <Text style={styles.touchableText}>Buy Now</Text>
       </TouchableOpacity>
     </View>
   );
@@ -27,11 +27,40 @@ const styles = StyleSheet.create({
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
   },
 
   priceWrapper: {
     display: 'flex',
     alignItems: 'center',
     gap: 8,
+  },
+
+  priceText: {
+    color: '#9B9B9B',
+    fontSize: 14,
+    fontWeight: '400',
+  },
+
+  priceNumber: {
+    color: '#C67C4E',
+    fontSize: 18,
+    fontWeight: '600',
+  },
+
+  touchable: {
+    width: 217,
+    height: 55,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#C67C4E',
+    borderRadius: 16,
+  },
+
+  touchableText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '600',
   },
 });
