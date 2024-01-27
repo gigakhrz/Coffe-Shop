9747c97d1ada5ab81cded670a8bed6e141d60687
gigakhrz <kharazishvilig13@gmail.com>
2024-01-27 16:12:44 +0400

    add some styles

--- components/detailsChilds/BuyNow.tsx
+++ components/detailsChilds/BuyNow.tsx
@@ -1,9 +1,28 @@
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
 const BuyNow = (): JSX.Element => {
-  return <View></View>;
+  return (
+    <View style={styles.wrapper}>
+      <View style={styles.priceWrapper}></View>
+      <TouchableOpacity>
+        <Text>Buy Now</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 export default BuyNow;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  wrapper: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+  },
+
+  priceWrapper: {
+    display: 'flex',
+    alignItems: 'center',
+    gap: 8,
+  },
+});
